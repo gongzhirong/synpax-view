f4a49b23db337e92962c9b3f4db9126bdea8e947
gongzhirong <gongzhirong@chinasie.com>
2019-01-15 13:57:26 +0800

    选择类型、选择袋子模块开发

--- src/views/selectBag.vue
+++ src/views/selectBag.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="selectBag">
     <PackageSteps />
-    <h1>选择包装</h1>
+    <div class="content">
+      <div class="body">
+        <div class="choseWrap">
+          <img src="../assets/img/type_4.png">
+          <div class="descBox">
+            <p>三边封</p>
+            <p>ORGAN BAG</p>
+            <p class="desc_title">袋形描述</p>
+          </div>
+          <p class="desc_content">SYNPAX由拥有30年行业经验的资深专家研发E-PACKAGING致力于简化定制包装流程。</p>
+        </div>
+        <div class="choseWrap">
+          <img src="../assets/img/type_4.png">
+          <div class="descBox">
+            <p>三边封</p>
+            <p>ORGAN BAG</p>
+            <p class="desc_title">袋形描述</p>
+          </div>
+          <p class="desc_content">SYNPAX由拥有30年行业经验的资深专家研发E-PACKAGING致力于简化定制包装流程。</p>
+        </div>
+        <div class="choseWrap">
+          <img src="../assets/img/type_4.png">
+          <div class="descBox">
+            <p>三边封</p>
+            <p>ORGAN BAG</p>
+            <p class="desc_title">袋形描述</p>
+          </div>
+          <p class="desc_content">SYNPAX由拥有30年行业经验的资深专家研发E-PACKAGING致力于简化定制包装流程。</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -19,5 +49,60 @@ export default {
 }
 </script>
 <style lang="less">
-.selectBag {}
+.selectBag {
+  .content {
+    background-color: #F5F5F5;
+
+    .body {
+      width: 1400px;
+      box-sizing: border-box;
+      padding: 40px 46px 40px;
+      background-color: #F9F9F9;
+      margin: 0 auto;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+
+      .choseWrap {
+        width: 600px;
+        padding: 100px 20px 45px;
+        margin-bottom: 40px;
+        background-color: #fff;
+        font-size: 26px;
+        text-align: center;
+        cursor: pointer;
+        background: linear-gradient(140deg, transparent 38px, #fff 0);
+
+        img {
+          width: 414px;
+        }
+
+        .descBox {
+          text-align: left;
+          margin: 0 30px;
+          padding: 0 0 12px;
+          border-bottom: 1px solid #707070;
+          position: relative;
+
+          .desc_title {
+            display: inline-block;
+            padding: 0 8px 12px 20px;
+            border-bottom: 5px solid #060606;
+            position: absolute;
+            top: 37px;
+            right: 0;
+          }
+        }
+
+        .desc_content {
+          text-align: left;
+          margin: 12px 30px;
+          font-size: 20px;
+          line-height: 22px;
+          color: #5A5D5F;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/views/selectType.vue
+++ src/views/selectType.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="selectType">
     <PackageSteps />
+    <div class="content">
+      <div class="header">
+        <p class="text">开始我的包装</p>
+        <div class="line"></div>
+      </div>
+      <div class="body">
+        <div class="choseWrap">
+          <img src="../assets/img/type_4.png">
+          <p>包装袋</p>
+          <p>PACKING BAG</p>
+        </div>
+        <div class="choseWrap picHandle">
+          <img src="../assets/img/type_4.png">
+          <p>包装膜</p>
+          <p>PACKING BAG</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -18,5 +36,52 @@ export default {
 }
 </script>
 <style lang="less">
-.selectType {}
+.selectType {
+  .content {
+    padding-top: 80px;
+    padding-bottom: 250px;
+    position: relative;
+    background-color: #F5F5F5;
+
+    .text {
+      position: absolute;
+      top: 60px;
+      font-size: 26px;
+      left: 50%;
+      margin-left: -58px;
+      font-weight: 600;
+      padding: 0 20px;
+      background-color: #f5f5f5;
+    }
+
+    .line {
+      width: 1400px;
+      height: 1px;
+      margin: 0 auto;
+      background-color: #aaa;
+    }
+
+    .body {
+      width: 1400px;
+      margin: 80px auto 0;
+      display: flex;
+      justify-content: space-around;
+
+      .choseWrap {
+        width: 386px;
+        padding: 100px 20px 45px;
+        border-radius: 50px;
+        background-color: #fff;
+        font-size: 26px;
+        text-align: center;
+        cursor: pointer;
+        background: linear-gradient(-45deg, transparent 15px, #58a 0);
+
+        img {
+          width: 414px;
+        }
+      }
+    }
+  }
+}
 </style>
