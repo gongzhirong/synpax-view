ca5bbb3d3b8fd56bed1318a0b6da1c9a1ee88f58
gongzhirong <gongzhirong@chinasie.com>
2019-01-16 11:44:54 +0800

    完成配置页代码上传

--- src/views/finish.vue
+++ src/views/finish.vue
@@ -2,10 +2,14 @@
   <div class="finish">
     <PackageSteps />
     <div class="content">
-      <div class="wrap">
+      <div class="content_wrap">
         <p>您已完成您的包装</p>
         <p>我们已收到您的包装配置</p>
         <p>我们会尽快为您安排供应商进行报价</p>
+      </div>
+      <div class="footer">
+        <p class="title">浏览器将在8秒后自动返回首页</p>
+        <a>如无反应，请点这里</a>
       </div>
     </div>
   </div>
@@ -27,11 +31,29 @@ export default {
 <style lang="less">
 .finish {
   background-color: #F5F5F5;
+  padding-bottom: 245px;
+
   .content {
     width: 1400px;
-    margin: 12px auto 0;
+    margin: 16px auto 0;
     background-color: white;
+    box-sizing: border-box;
     padding: 50px;
+    background: linear-gradient(140deg, transparent 38px, #fff 0);
+    text-align: center;
+
+    .content_wrap {
+      font-size: 26px;
+    }
+
+    .footer {
+      margin-top: 60px;
+      font-size: 18px;
+      
+      a {
+        color: #7C8290;
+      }
+    }
   }
 }
 </style>
