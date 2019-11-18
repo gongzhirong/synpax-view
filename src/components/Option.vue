516139183e4f4daea1b782566e6a27daea290b51
gongzhirong <283203806@qq.com>
2019-11-19 04:43:24 +0800

    文件名称更改

---
+++ src/components/Option.vue
@@ -0,0 +1,33 @@
+<template>
+  <li class="selectOtion" @click="selected" :data-value="value">{{label}}</li>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: [Number, String],
+      default () {
+        return ''
+      }
+    },
+    label: {
+      type: [Number, String],
+      default () {
+        return ''
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    selected () {
+      this.$parent.$emit('selected', {value: this.value, label: this.label})
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+li { list-style: none; }
+</style>
